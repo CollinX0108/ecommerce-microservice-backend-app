0db86fc40d580d095e69f262494b080c1662ec28
CollinX0108 <collin0108@hotmail.com>
2025-06-14 23:12:00 -0500

    fix: AKS

--- terraform/environments/master/terraform.tfvars
+++ terraform/environments/master/terraform.tfvars
@@ -1,4 +1,4 @@
 environment = "master"
 location    = "eastus"
 node_count  = 1
-node_size   = "Standard_B2s" 
+node_size   = "Standard_B2ps_v2" 
--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -5,9 +5,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix         = var.dns_prefix
 
   default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_size
+    name                          = "default"
+    node_count                    = var.node_count
+    vm_size                       = var.node_size
+    temporary_name_for_rotation   = "defaulttemp"
   }
 
   identity {
@@ -18,6 +19,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin = "kubenet"
     network_policy = "calico"
   }
+
+  tags = {
+    Environment = var.environment
+    Project     = "ecommerce-microservice"
+    Org         = "1110285872"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "arm64" {
+  name                  = "arm64"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  node_count           = var.node_count
+  vm_size              = "Standard_B2ps_v2"
+  node_taints          = ["arch=arm64:NoSchedule"]
 
   tags = {
     Environment = var.environment
